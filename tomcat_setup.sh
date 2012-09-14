bf7b0c07bd2a7b0e4ca618611196478fd659dbbe
Ruekov <guillemrueda@gmail.com>
2012-09-14 10:52:48 +0200

    Best downloading Apache Tomcat link added

--- tomcat_setup.sh
+++ tomcat_setup.sh
@@ -49,8 +49,13 @@ case user_version in
 "1") 
 	echo "Installing tomcat 7.0.30"
 	versionbar="7.0.30"
-	# Fastest download link for Ukraine to get tomcat
-	wget http://apache-mirror.telesys.org.ua/tomcat/tomcat-7/v7.0.30/bin/apache-tomcat-7.0.30.tar.gz
+	if [ -x /usr/bin/lynx ] || [ -x /usr/sbin/lynx ];then
+		echo "Lynx is installed, now asking for the best download place"
+		wget $(lynx --dump http://tomcat.apache.org/download-70.cgi | grep -o "http:.*" | sed -e '\/apache-tomcat-7.0.30.tar.gz$/!d')
+		else
+		# Fastest download link for Ukraine to get tomcat
+		wget http://apache-mirror.telesys.org.ua/tomcat/tomcat-7/v7.0.30/bin/apache-tomcat-7.0.30.tar.gz
+	fi
 	;;
 "2") 
 	echo "Installing tomcat 7.0.29"
@@ -61,14 +66,24 @@ case user_version in
 "3") 
 	echo "Installing tomcat 6.0.35"
 	versionbar="6.0.35"
-	# Fastest download link for Ukraine to get tomcat
-	wget http://apache.vc.ukrtel.net/tomcat/tomcat-6/v6.0.35/bin/apache-tomcat-6.0.35.tar.gz
+	if [ -x /usr/bin/lynx ] || [ -x /usr/sbin/lynx ];then
+		echo "Lynx is installed, now asking for the best download place"
+		wget $(lynx --dump http://tomcat.apache.org/download-60.cgi | grep -o "http:.*" | sed -e '\/apache-tomcat-6.0.35.tar.gz$/!d')
+		else
+		# Fastest download link for Ukraine to get tomcat
+		wget http://apache.vc.ukrtel.net/tomcat/tomcat-6/v6.0.35/bin/apache-tomcat-6.0.35.tar.gz
+	fi
 	;;
 "4")
 	echo "Installing tomcat 5.5.35"
 	versionbar="5.5.35"
-	# Fastest download link for Ukraine to get tomcat	
-	wget http://apache.cp.if.ua/tomcat/tomcat-5/v5.5.35/bin/apache-tomcat-5.5.35.tar.gz
+	if [ -x /usr/bin/lynx ] || [ -x /usr/sbin/lynx ];then
+		echo "Lynx is installed, now asking for the best download place"
+		wget $(lynx --dump http://tomcat.apache.org/download-55.cgi | grep -o "http:.*" | sed -e '\/apache-tomcat-5.5.35.tar.gz$/!d')
+		else
+		# Fastest download link for Ukraine to get tomcat	
+		wget http://apache.cp.if.ua/tomcat/tomcat-5/v5.5.35/bin/apache-tomcat-5.5.35.tar.gz
+	fi
 	;;
 *)	
 	echo "You must give choice between 1-4"
