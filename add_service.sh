51c221a3b268a99cb3b406b7d36caae47eb9b32e
Ruekov <guillemrueda@gmail.com>
2012-09-19 09:44:01 +0200

    added new lib

---
+++ add_service.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+############################################
+#           Adding new service             #
+############################################
+
+# Security copy
+
+if [ $# != 3 ]; then
+	echo "you must give 3 arguments: user, user_domain, route to tomcat app from USER path"
+	exit 0
+fi
+
+$1=user_tomcat
+$2=user_domain
+$3=tomcat_app
+
+# copy of configfile
+
+cp /usr/local/apache/conf/userdata/std/2/$user_tomcat/$user_domain/cp_jkmount.conf  /usr/local/apache/conf/userdata/std/2/$user_tomcat/$user_domain/cp_jkmount.conf.old1
+cp /usr/local/apache/conf/userdata/std/1/$user_tomcat/$user_domain/cp_jkmount.conf  /usr/local/apache/conf/userdata/std/1/$user_tomcat/$user_domain/cp_jkmount.conf.old1
+cp /usr/local/apache/conf/userdata/ssl/2/$user_tomcat/$user_domain/cp_jkmount.conf  /usr/local/apache/conf/userdata/ssl/2/$user_tomcat/$user_domain/cp_jkmount.conf.old1
+cp /usr/local/apache/conf/userdata/ssl/1/$user_tomcat/$user_domain/cp_jkmount.conf  /usr/local/apache/conf/userdata/ssl/1/$user_tomcat/$user_domain/cp_jkmount.conf.old1
+
+# adding new route to services
+
+sed -i 's/<\/IfModule>/JkMount ${tomcat_app//'/'/'\/'} $user_tomcat \n<\/IfModule>/g'
